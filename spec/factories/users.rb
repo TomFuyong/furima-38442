df2b54b553154454de66d4abf6b8b3f2482ec45e
TomFuyong <j8211649@gmail.com>
2022-10-06 14:29:15 +0900

    ユーザー管理機能

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname { 'test' }
+    email { 'test@example' }
+    password { 'test1234' }
+    password_confirmation { password }
+    last_name { '鈴木' }
+    first_name { '一郎' }
+    last_name_kana { 'スズキ' }
+    first_name_kana { 'イチロウ' }
+    birth_date { '1930-01-01' }
+  end
+end
